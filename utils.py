e0361bc0a864d94885a9a845749a3f470c126c8e
Tosin Damilare James Animashaun <acetakwas@gmail.com>
2016-04-04 17:08:49 +0100

    Add new feature to publish audit result to spreadsheet

--- utils.py
+++ utils.py
@@ -16,9 +16,29 @@ def get_rows(start_index, stop_index):
     for row in range(start_index, stop_index):
         row_data = []
 
-        for col in range(3, 6):
-            row_data.append(sheet.cell(row=row, column=col).value)
+        for col in range(1, 3):
+            row_data.append(str(sheet.cell(row=row, column=col).value))
+        row_data.append(0)
         
-        all_data.append(tuple(row_data))
+        #all_data.append(tuple(row_data))
+        all_data.append(row_data)
 
     return all_data    
+
+
+
+def load_db_to_sheet():
+    from audit import db_ops
+
+    new_workbook = openpyxl.Workbook()
+    new_worksheet = new_workbook.active
+
+    all_data = db_ops.ret_all(db_ops.Record)
+
+    for data in all_data:
+        row = [data.isbn, data.name, data.units]
+        new_worksheet.append(row)
+
+    new_workbook.save('audit_output_sheet.xlsx')
+
+# 7 - 5875
